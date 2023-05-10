ce4361900dc8818aa707b448251c004459d19081
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-05-10 11:33:26 +0700

    memasukan void siswa dan orang

--- 067_FriendClass/067_FriendClass.cpp
+++ 067_FriendClass/067_FriendClass.cpp
@@ -11,4 +11,23 @@ public:
 	friend class siswa;
 };
 
+class siswa {
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
 
